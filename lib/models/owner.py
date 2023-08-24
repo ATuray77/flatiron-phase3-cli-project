601e8f1198ae53c1c5ebe48006ce4ea9eae23d25
Aruna Turay <tura_arun@yahoo.com>
2023-08-24 15:46:27 -0400

    changed Users to Owners

--- lib/models/owner.py
+++ lib/models/owner.py
@@ -2,18 +2,18 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from .base import Base
 
-class User(Base):
-    __tablename__ = "users"
+class Owner(Base):
+    __tablename__ = "owners"
     id = Column(Integer, primary_key = True)
     username = Column(String, nullable=False )
     name = Column(String, nullable = False)
     phone= Column(String, nullable = False)
     email= Column(String, unique=True)
 
-    cars = relationship("Car", backref="user") # relationship
+    cars = relationship("Car", backref="owner") # relationship
 
 def __repr__(self):
-    return f"\n<Artist" \
+    return f"\n<Owner" \
         + f"id={self.id}, " \
         + f"username={self.username}, " \
         + f"name={self.name}, " \

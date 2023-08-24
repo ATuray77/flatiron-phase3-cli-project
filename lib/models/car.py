640d2d2a3f41177070844e7f93ca4736c47222a6
Aruna Turay <tura_arun@yahoo.com>
2023-08-24 15:47:41 -0400

    modifies Car class with

--- lib/models/car.py
+++ lib/models/car.py
@@ -7,11 +7,11 @@ class Car(Base):
     make = Column(String, nullable = False)
     model = Column(String, nullable = False)
     color = Column(String, nullable = False)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("owners.id"))
 
 
     def __repr__(self):
-        return f"\n<Song" \
+        return f"\n<Car" \
             + f"id={self.id}, " \
             + f"make={self.make}, " \
             + f"model={self.model}, " \

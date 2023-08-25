d3d575005264234b1cc8ae990bb2aff3a9bbc6fb
Aruna Turay <tura_arun@yahoo.com>
2023-08-25 19:31:04 -0400

    adds some coloring to the menu options

--- lib/cli.py
+++ lib/cli.py
@@ -1,18 +1,16 @@
 from simple_term_menu import TerminalMenu
-from ipdb import ipdb; ipdb.set_trace()
+from prettycli import red, yellow, green
+#import ipdb; ipdb.set_trace()
 
 def main():
-    print("WELCOME TO TURAY'S PARKING GARAGE!\n")
+    print(yellow("WELCOME TO TURAY'S PARKING GARAGE!\n"))
     options = ["Car Owner", "New Customer", "Add a car", "Exit!"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
-    user_input = input("Please make a selection")
-    print(f"You have selected {options[menu_entry_index]}!")
+    #user_input = input("Please make a selection")
+    print(green(f"You have selected {options[menu_entry_index]}!"))
 
-    handle_user_input(user_input)
-
-    def handle_user_input(input):
-        import ipdb; ipdb.set_trace()
+    
 
 
 if __name__ == "__main__":
@@ -26,6 +24,13 @@ if __name__ == "__main__":
 #     print("3. Exit")
 #     user_input = input("Please make a selection (1-3)")
 
+    # handle_user_input(user_input)
 
-    
+    # def handle_user_input(input):
+    #    is_number = input.isdigit()
+    #    if is_number:
+    #        selection = int(input)
+    #        import ipdb; ipdb.set_trace()
+
+
 # start()

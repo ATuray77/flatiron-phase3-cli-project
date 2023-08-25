b8b8f96798eff247e3ab91936dc86d2e805fd54f
Aruna Turay <tura_arun@yahoo.com>
2023-08-25 18:37:55 -0400

    simple terminal menu implementing

--- lib/cli.py
+++ lib/cli.py
@@ -1,10 +1,21 @@
 from simple_term_menu import TerminalMenu
 
 def main():
-    options = ["entry 1", "entry 2", "entry 3"]
+    print("WELCOME TO TURAY'S PARKING GARAGE!\n")
+    options = ["Car Owner", "New Customer", "Exit!"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     print(f"You have selected {options[menu_entry_index]}!")
 
 if __name__ == "__main__":
     main()
+
+
+# def start():
+#     print("WELCOME TO TURAY'S PARKING GARAGE!\n")
+#     print("1. My Toys")
+#     print("2. Add Toy")
+#     print("3. Exit")
+#     user_input = input("Please make a selection (1-3)")
+    
+# start()

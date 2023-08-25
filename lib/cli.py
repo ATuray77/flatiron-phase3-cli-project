f8e84930be555f539a2cbbcc2d3f082ff08d8610
Aruna Turay <tura_arun@yahoo.com>
2023-08-25 18:57:04 -0400

    import ipdb into cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,19 @@
 from simple_term_menu import TerminalMenu
+from ipdb import ipdb; ipdb.set_trace()
 
 def main():
     print("WELCOME TO TURAY'S PARKING GARAGE!\n")
     options = ["Car Owner", "New Customer", "Add a car", "Exit!"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
+    user_input = input("Please make a selection")
     print(f"You have selected {options[menu_entry_index]}!")
+
+    handle_user_input(user_input)
+
+    def handle_user_input(input):
+        import ipdb; ipdb.set_trace()
+
 
 if __name__ == "__main__":
     main()
@@ -17,5 +25,7 @@ if __name__ == "__main__":
 #     print("2. Add Toy")
 #     print("3. Exit")
 #     user_input = input("Please make a selection (1-3)")
+
+
     
 # start()
